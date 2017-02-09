70aa8341fc9f5d1dfa35b177e9e42bc688d5b6b5
dnABic (Andreja Babic) <ababic@gmail.com>
2017-02-09 14:10:55 +0100

    Initial commit

---
+++ step_snapshot_ebs_volumes.go
@@ -0,0 +1,117 @@
+package ebs
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/builder/amazon/common"
+	awscommon "github.com/mitchellh/packer/builder/amazon/common"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
+)
+
+type stepSnapshotEBSVolumes struct {
+	VolumeRunTags    map[string]string
+	Ctx              interpolate.Context
+	VolumeDoSnapshot bool
+}
+
+func (s *stepSnapshotEBSVolumes) Run(state multistep.StateBag) multistep.StepAction {
+	ec2conn := state.Get("ec2").(*ec2.EC2)
+	instance := state.Get("instance").(*ec2.Instance)
+	sourceAMI := state.Get("source_image").(*ec2.Image)
+	ui := state.Get("ui").(packer.Ui)
+
+	if s.VolumeDoSnapshot == false {
+		return multistep.ActionContinue
+	}
+
+	volumeIds := make([]*string, 0)
+	for _, v := range instance.BlockDeviceMappings {
+		if ebs := v.Ebs; ebs != nil {
+			volumeIds = append(volumeIds, ebs.VolumeId)
+		}
+	}
+
+	if len(volumeIds) == 0 {
+		return multistep.ActionContinue
+	}
+
+	var snapshotIdList []string
+	for _, v := range volumeIds {
+		createSnapResp, err := ec2conn.CreateSnapshot(&ec2.CreateSnapshotInput{
+			VolumeId: &v,
+		})
+		if err != nil {
+			err := fmt.Errorf("Error while creating snapshot of EBS Volume %s: %s", v, err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+
+		snapshotId := *createSnapResp.SnapshotId
+		snapshotIdList = append(snapshotIdList, snapshotId)
+		ui.Say(fmt.Sprintf("Creating snapshot of volume %s with ID %s", v, snapshotId))
+
+		stateChange := awscommon.StateChangeConf{
+			Pending: []string{"pending"},
+			Target:  "completed",
+			Refresh: func() (interface{}, string, error) {
+				resp, err := ec2conn.DescribeSnapshots(&ec2.DescribeSnapshotsInput{SnapshotIds: []*string{&snapshotId}})
+				if err != nil {
+					return nil, "", err
+				}
+
+				if len(resp.Snapshots) == 0 {
+					return nil, "", errors.New("No snapshots found.")
+				}
+
+				s := resp.Snapshots[0]
+				return s, *s.State, nil
+				//return nil, "", nil
+			},
+			StepState: state,
+		}
+
+		ui.Say("Waiting for EBS Volume snapshot to complete...")
+		if _, err := awscommon.WaitForState(&stateChange); err != nil {
+			err := fmt.Errorf("Error waiting for EBS Volume snapshot: %s", err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+
+	}
+
+	if len(s.VolumeRunTags) == 0 {
+		return multistep.ActionContinue
+	}
+
+	ui.Say("Adding tags to EBS Volumes Snapshots")
+	tags, err := common.ConvertToEC2Tags(s.VolumeRunTags, *ec2conn.Config.Region, *sourceAMI.ImageId, s.Ctx)
+	if err != nil {
+		err := fmt.Errorf("Error tagging EBS Volumes Snapshots: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	_, err = ec2conn.CreateTags(&ec2.CreateTagsInput{
+		Resources: snapshotIdList,
+		Tags:      tags,
+	})
+
+	if err != nil {
+		err := fmt.Errorf("Error tagging EBS Volume Snapshots: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	return multistep.ActionContinue
+}
+
+func (s *stepSnapshotEBSVolumes) Cleanup(state multistep.StateBag) {
+	// No cleanup...
+}
